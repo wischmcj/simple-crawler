11e7f513fba15350cc01b19ecf1a4f89ca52fcec
Collin Wischmcj <cjwischmeyer@gmail.com>
2025-05-18 13:16:05 -0400

    adding non config based logger to tests for git actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
+import logging
 import os
 import sys
 
 cwd = os.getcwd()
 
-from simple_crawler.config.configuration import get_logger  # noqa
+# from mr_crawly.config.configuration import get_logger  # noqa
 
-logger = get_logger(__name__)
+# logger = get_logger(__name__)
+logger = logging.getLogger(__name__)
 
 # Read in environment variables, set defaults if not present
 root_loc = os.path.dirname(__file__)
