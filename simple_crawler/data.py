47dd9b453c24b9b959b8f42d94e74b7872b03d9c
wischmcj <cjwischmeyer@gmail.com>
2025-05-21 12:19:27 -0400

    making changes needed to base data table class, updating tests partially

--- simple_crawler/data.py
+++ simple_crawler/data.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
+import asyncio
 import json
 import sqlite3
+from collections import defaultdict
 
+import aiosqlite
 from config.configuration import _get_table_details, get_logger
+from redis import asyncio as redis
+from utils import deserialize
 
 logger = get_logger("data")
 
@@ -28,10 +33,11 @@ class BulkDBWriter:
 
     async def flush_data(self, table_name):
         """Builds and insert query and executes it"""
-        logger.info("Flushing data...")
-        tables = [(table_name, self.tables[table_name])] 
         if table_name == "all":
             tables = self.to_write.items()
+        else:
+            tables = [(table_name, self.tables[table_name])]
+        print("Flushing data...")
 
         for table_name, data in tables:
             table = self.tables[table_name]
@@ -45,13 +51,13 @@ class BulkDBWriter:
             logger.debug(f"Received message: {message}")
             if message is not None:
                 if message["data"] == STOPWORD:
+                    await self.flush_data("all")
                     self.running = False
                     break
                 else:
                     request = await deserialize(message.get("data", ""))
                     kwargs = json.loads(request)
                     await self.store_data(**kwargs)
-        await self.flush_data("all")
 
 
 class DatabaseManager:
@@ -61,93 +67,142 @@ class DatabaseManager:
         self.tables = {}
         self._init_db()
 
-    def _init_db(self):
+    async def _init_db(self):
         # Initialize databases
         self.table_details = _get_table_details()
-        self.create_tables()
+        await self.create_tables()
         missing_tables = set(self.tables.keys()) - {"runs", "urls", "sitemaps"}
         if missing_tables:
             raise Exception(f"Missing tables: {missing_tables}")
         self.listeners = []
         self.futures = []
-        self.add_listener(BulkDBWriter, (self.tables,))
+        await self.add_listener(BulkDBWriter, (self.tables,))
 
-    def shutdown(self):
+    async def shutdown(self):
         """Shutdown the database manager"""
         logger.info("Shutting down database manager")
-        self.redis_conn.publish("writer", STOPWORD)
+        await self.redis_conn.publish("writer", STOPWORD)
+        await asyncio.gather(*self.futures)
 
-    def add_listener(self, listener_cls, args=()):
+    async def add_listener(self, listener_cls, args=()):
         pubsub = self.redis_conn.pubsub()
-        pubsub.subscribe("writer")
+        await pubsub.subscribe("writer")
         writer = listener_cls(*args)
-        asyncio.run(writer.handle_message(pubsub))
+        future = asyncio.create_task(writer.handle_message(pubsub))
         self.listeners.append(writer)
+        self.futures.append(future)
 
-    def create_tables(self):
+    async def create_tables(self):
         for details in self.table_details:
             table = BaseTable(**details)
-            table.db_operation(operation="create")
+            await table.db_operation(operation="create")
             self.tables[table.table_name] = table
+
+    async def start_run(self, run_id: str, seed_url: str, max_pages: int) -> int:
+        """Start a new crawl run"""
+        data = {
+            "run_id": run_id,
+            "seed_url": seed_url,
+            "max_pages": max_pages,
+            "event": "start_run",
+        }
+        await self.redis_conn.publish(
+            "writer", json.dumps({"table_name": RUNS_TABLE, "data": data})
+        )
+
+    async def complete_run(self, run_id: str, seed_url: str, max_pages: int):
+        """Mark a run as completed and set end time"""
+        data = {
+            "run_id": run_id,
+            "seed_url": seed_url,
+            "max_pages": max_pages,
+            "event": "complete_run",
+        }
+        logger.info(f"Completing run {run_id}")
+        await self.redis_conn.publish(
+            "writer", json.dumps({"table_name": RUNS_TABLE, "data": data})
+        )
 
 
 class BaseTable:
     def __init__(
         self,
-        conn,
+        db_file,
         table_name: str = "",
         columns: list[str] = ["id", "col1", "col2", "col3"],
         types: list[str] = ["INTEGER", "INTEGER", "TEXT", "TEXT"],
         primary_key: str = "id",
         unique_keys: list[str] = ["id"],
     ):
-        self.conn = conn
-        self.cursor = self.conn.cursor()
+        self.db_file = db_file
         self.table_name = table_name
         self.columns = columns
         self.types = types
         self.primary_key = primary_key
         self.unique_keys = unique_keys
+        self.string_functions = {
+            "insert": self.build_insert_string,
+            "update": self.build_update_string,
+            "create": self.build_create_string,
+        }
 
-    def build_create_string(self, *args):
+    async def build_create_string(self, *args):
         """Build a create string for a table"""
         cols_to_types = dict(zip(self.columns, self.types))
 
         if len(self.unique_keys) == 0:
             self.unique_keys = [self.primary_key]
         unique_cols = ", ".join(self.unique_keys)
-        pk = self.primary_key
-        pk_row = {pk: f"{cols_to_types[pk]} PRIMARY KEY AUTOINCREMENT"}
-        cols_to_types.update(pk_row)
         cols_w_types = [f"{col} {ctype}" for col, ctype in cols_to_types.items()]
-
         create_cols_string = ", ".join(cols_w_types)
         create_string = f"""CREATE TABLE IF NOT EXISTS {self.table_name} (
                                     {create_cols_string},
                                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                                     UNIQUE({unique_cols})
                                 )"""
-        logger.debug(create_string)
-        return create_string
+        return create_string, ()
 
-    def create_table(self):
+    async def build_insert_string(self, data: list[dict]):
+        columns = [k for k, v in data[0].items() if k not in ["id"]]
+        params = [tuple(row.get(col, "") for col in columns) for row in data]
+        placeholders = ",".join(["?" for _ in columns])
+        select_list = ",".join(columns)
+        insert_query = (
+            f"INSERT INTO {self.table_name} ({select_list}) VALUES ({placeholders})"
+        )
+        return insert_query, params
+
+    async def build_update_string(self, data: list[dict]):
+        columns = [k for k, v in data[0].items() if k not in ["id", "created_at"]]
+        params = [tuple(row.get(col, "") for col in columns) for row in data]
+        placeholders = ", ".join([f"{col} = ?" for col in columns])
+        placeholders.append(data[0]["id"])
+        query = f"""UPDATE {self.table_name}
+              SET {placeholders}
+              WHERE {self.primary_key} = ?"""
+        return query, params
+
+    async def db_operation(self, data: list[dict] = None, operation: str = "insert"):
         """Create a table if it doesn't exist"""
-        create_string = self.build_create_string()
-        self.cursor.execute(create_string)
-        self.conn.commit()
+        query, params = await self.string_functions[operation](data)
+        result = await self.execute_query(query, params=params)
+        if operation == "insert" and result is False:
+            self.db_operation(data, "update")
+            logger.warning(f"Row already exists, updated {self.table_name}")
+        return result
 
-    def execute_query(
-        self, query: str, params: tuple = (), return_results: bool = False
-    ):
+    async def execute_query(self, query: str, params: tuple | list[tuple] = ()):
         """Execute a query"""
-        logger.debug(f"Executing query: {query}")
-        for i in range(3):
+        logger.debug(f"Executing query: {query} w/ params: {params}")
+        for _ in range(3):
             try:
-                self.cursor.execute(query, params)
-                self.conn.commit()
-                return True
+                async with aiosqlite.connect(self.db_file) as db:
+                    func = db.executemany if isinstance(params, list) else db.execute
+                    await func(query, params)
+                    await db.commit()
+                    return True
             except sqlite3.OperationalError as e:
                 logger.error(f"Integrity error: {e}")
-                self.conn.rollback()
-                time.sleep(1)
+                await asyncio.sleep(1)
         return False
+
